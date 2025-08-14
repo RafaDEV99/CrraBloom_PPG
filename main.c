96cf2577099d1cf5ddae1b54673338078175396e
RafaDEV99 <162469453+RafaDEV99@users.noreply.github.com>
2025-08-14 13:39:38 +0200

    Created the main file and CMakeLists.txt
    Cool, a great start!

---
+++ main.c
@@ -0,0 +1,242 @@
+/**********************************
+ *
+ * CraBOOM engine THE epic physics program- Legacy code
+ * Code by: @RafaDEV99 (Rafael Tangyan Guskov) :3
+ *
+ *********************************/
+
+#include <raylib.h>
+#include <raymath.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// External include (from the include folder :P )
+#define PHYSAC_IMPLEMENTATION
+#include "Includes/physac.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "Includes/raygui.h"
+
+// All defines and macros goes here
+#define WINDOW_WIDTH   1080
+#define WINDOW_HEIGHT  700
+#define WINDOW_TITLE   "CraBOOM engine - Raylib"
+
+#define GRAVITY_Y  9.8f
+#define GRAVITY_X  0.0f
+
+// Enums
+enum ObjectType
+{
+    RECTANGLE_BODY,
+    CIRCLE_BODY,
+    POLYGON_BODY,
+};
+
+// Function yoinked from the Physac examples :3
+void DrawCollisionWireFrames(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        PhysicsBody body = GetPhysicsBody(i);
+
+        if (body != NULL)
+        {
+            int vertexCount = GetPhysicsShapeVerticesCount(i);
+            for (int j = 0; j < vertexCount; j++)
+            {
+                // Get physics bodies shape vertices to draw lines
+                // Note: GetPhysicsShapeVertex() already calculates rotation transformations
+                Vector2 vertexA = GetPhysicsShapeVertex(body, j);
+
+                int jj = (((j + 1) < vertexCount) ? (j + 1) : 0);   // Get next vertex or first to close the shape
+                Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
+
+                DrawLineV(vertexA, vertexB, DARKGRAY);     // Draw a line between two vertex positions
+            }
+        }
+
+    }
+}
+
+const char* GetBodyTypeName(int bodyIndex)
+{
+    switch (bodyIndex) 
+    {
+        case RECTANGLE_BODY:
+            return "Rectangle";
+        case CIRCLE_BODY:
+            return "Circle";
+        case POLYGON_BODY:
+            return "Polygon";
+        default:
+            return "MissingNo";
+            break;
+    }
+}
+
+// TODO: add more to this function :P
+void DrawBodySubGui(int valueBodyIndex, float x_position, float y_position)
+{
+    // NOTE: When the Object is selected (or just created), it will show a diferent GUI
+    // WARNING: Its better to use the enum the provided in the enums section (You can olso include a new instanciator function)
+
+    const char* ObjectName = GetBodyTypeName(valueBodyIndex);
+
+    float WBoxWidth = 200.0f;
+    float WBoxHeight = 200.0f;
+
+    GuiWindowBox((Rectangle){x_position, y_position, WBoxWidth, WBoxHeight}, TextFormat("%s properties", ObjectName));
+
+    // switch (valueBodyIndex) 
+    // {
+    //     case RECTANGLE_BODY:
+    //         break;
+    //     case CIRCLE_BODY:
+    //         break;
+    //     case POLYGON_BODY:
+    //         break;
+    //     default:
+    //         break;
+    // }
+}
+
+float IncreseXGravity(float gravityValue)
+{
+    return gravityValue += 0.1f;
+}
+
+int main()
+{
+    // Initialization functions
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    InitPhysics();
+    SetTargetFPS(60);
+
+    // NOTE: Init varibles
+    float delta;
+    bool buttonFlag = false;
+
+    PhysicsBody bodiesList[PHYSAC_MAX_BODIES] = { 0 };
+    Rectangle BodieRect[PHYSAC_MAX_BODIES] = { 0 };
+    
+    bool DropEdit = false; // <-- Drop down menu 1
+    int ValueDrop = RECTANGLE_BODY;
+
+    // Here I store the value inserted in the value boxes X and Y:
+    int ValueGetX = 0;
+    int ValueGetY = 0;
+
+    int GetDencity = 0;
+    int BodiesCount = 0;
+
+    // Booleans for managing the state of two value boxes:
+    bool editValX = false;
+    bool editValY = false;
+
+    bool dencityEdit = false;
+
+    float gravityX = 0.0f;
+    float gravityY = 0.0f;
+
+    Vector2 MousePosition = { 0.0f, 0.0f };
+
+    while (!WindowShouldClose()) 
+    {
+        // Update values
+        delta = GetFrameTime();
+        BodiesCount = GetPhysicsBodiesCount();
+        MousePosition = GetMousePosition();
+
+        gravityX = IncreseXGravity(0.0f);
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            float force = 1.0f;
+            BodiesCount -= 1;
+            PhysicsShatter(bodiesList[BodiesCount], MousePosition, force);
+        }
+
+        printf("gravityX: %f\n", gravityX);
+        DrawText(TextFormat("gravityX: %f", gravityX), 540, 10, 20, DARKGRAY);
+
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("CraBOOM engine - Alpha 1", 10, 40, 20, DARKGRAY);
+            DrawText("Press ESC to exit", 300, 40, 20, DARKGRAY);
+            DrawText(TextFormat("Bodies: %d", BodiesCount), 950, 10, 20, DARKGRAY);
+            DrawFPS(10, 10);
+            DrawCollisionWireFrames(BodiesCount);
+            if (GuiButton((Rectangle){10, 90, 120, 50}, "#8#add body"))
+            {
+                buttonFlag = !buttonFlag;
+            }
+            
+            DrawBodySubGui(ValueDrop, 10, 490);
+
+            if (buttonFlag == true)
+            {
+                if (GuiWindowBox((Rectangle){10, 170, 300, 300}, "Body Creation"))
+                {
+                    buttonFlag = !buttonFlag;
+                }
+
+                // Gui Value boxes layer
+                if (GuiValueBox((Rectangle){30, 270, 120, 40}, "x ", &ValueGetX, 0, WINDOW_WIDTH, editValX))
+                    editValX = !editValX;
+                if (GuiValueBox((Rectangle){170, 270, 120, 40}, "y ", &ValueGetY, 0, WINDOW_HEIGHT, editValY))
+                    editValY = !editValY;
+                if (GuiValueBox((Rectangle){30, 330, 120, 40}, " ", &GetDencity, 0.0f, 500.0f, dencityEdit))
+                    dencityEdit = !dencityEdit;
+                if (GuiDropdownBox((Rectangle){30, 210, 120, 40}, "Rectangle;Circle;Polygon", &ValueDrop, DropEdit))
+                    DropEdit = !DropEdit;
+
+                GuiLabel((Rectangle){165, 335, 60, 25}, "<-- Dencity");
+
+                if (GuiButton((Rectangle){30, 390, 120, 50}, "CREATE!!!")) // <-- If the button is true do the next:
+                {
+                    PhysicsBody newBody;
+                    Vector2 BVector = {ValueGetX, ValueGetY}; // <-- Body Vector int
+
+                    // Temp values while I cretae all the GUI for this values
+                    int RectHeight = 40;
+                    int RectWidth = 300;
+
+                    int circleRad = 25;
+
+                    int PolySides = 5;
+                    int PolyRad = 25;
+
+                    // Create a body based on the value of the drop down menu
+                    switch (ValueDrop) 
+                    {
+                        case RECTANGLE_BODY:
+                            newBody = CreatePhysicsBodyRectangle(BVector, RectWidth, RectHeight, GetDencity);
+                            break;
+                        case CIRCLE_BODY:
+                            newBody = CreatePhysicsBodyCircle(BVector, circleRad, GetDencity);
+                            break;
+                        case POLYGON_BODY:
+                            newBody = CreatePhysicsBodyPolygon(BVector, PolyRad, PolySides, GetDencity);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (newBody != NULL)
+                        bodiesList[BodiesCount] = newBody;
+
+                }
+
+            }
+            
+        EndDrawing();
+    }
+    
+    ClosePhysics();
+    CloseWindow();
+    return 0;
+}
+
