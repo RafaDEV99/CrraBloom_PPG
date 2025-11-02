58e450a9c1ac46a036ea7947d03b96c131ec99be
RafaDEV99 <162469453+RafaDEV99@users.noreply.github.com>
2025-11-02 21:49:55 +0100

    Moved all the code for the fist window to a function

--- test/UITest.c
+++ test/UITest.c
@@ -10,6 +10,36 @@
 
 struct nk_context *ctx;
 
+void FirstWindowPannel(struct nk_context *ctx, char windowTittle[], float dencity, int windowFlags, struct nk_colorf cubeColor)
+{
+    if (nk_begin_titled(ctx, "ObjWindow", windowTittle, nk_rect(20, 75, 340, 320), windowFlags))
+    {
+        nk_layout_row_static(ctx, 35, 340, 1);
+        nk_label_wrap(ctx, "Welcome to the CrraBloom creation window!");
+
+        nk_layout_row_static(ctx, 30, 250, 1);
+        nk_slider_float(ctx, 0.0f, &dencity, 100.0f, 0.1f);
+
+        nk_layout_row_begin(ctx, NK_STATIC, 30, 1);
+        {
+            nk_layout_row_push(ctx, 100);
+            nk_label(ctx, "First Row", NK_TEXT_LEFT);
+
+            nk_layout_row_push(ctx, 100);
+            nk_label(ctx, "Second Row", NK_TEXT_LEFT);
+        }
+        nk_layout_row_end(ctx);
+
+        nk_layout_row_dynamic(ctx, 60, 2);
+        nk_button_label(ctx, "Button 1");
+        nk_button_label(ctx, "Button 2");
+
+        nk_layout_row_static(ctx, 120, 120, 1);
+        cubeColor = nk_color_picker(ctx, cubeColor, NK_RGBA);
+    }
+    nk_end(ctx);
+}
+
 int main()
 {
     InitWindow(1080, 700, "Raygui example");
@@ -33,32 +63,7 @@ int main()
 
         // printf("The object was: %f dencity\n", testDencity);
 
-        if (nk_begin_titled(ctx, "ObjWindow", windowTittle, nk_rect(20, 75, 340, 320), windowFlags))
-        {
-            nk_layout_row_static(ctx, 35, 340, 1);
-            nk_label_wrap(ctx, "Welcome to the CrraBloom creation window!");
-
-            nk_layout_row_static(ctx, 30, 250, 1);
-            nk_slider_float(ctx, 0.0f, &testDencity, 100.0f, 0.1f);
-
-            nk_layout_row_begin(ctx, NK_STATIC, 30, 1);
-            {
-                nk_layout_row_push(ctx, 100);
-                nk_label(ctx, "First Row", NK_TEXT_LEFT);
-
-                nk_layout_row_push(ctx, 100);
-                nk_label(ctx, "Second Row", NK_TEXT_LEFT);
-            }
-            nk_layout_row_end(ctx);
-
-            nk_layout_row_dynamic(ctx, 60, 2);
-            nk_button_label(ctx, "Button 1");
-            nk_button_label(ctx, "Button 2");
-
-            nk_layout_row_static(ctx, 120, 120, 1);
-            cubeColor = nk_color_picker(ctx, cubeColor, NK_RGBA);
-        }
-        nk_end(ctx);
+        FirstWindowPannel(ctx, windowTittle, testDencity, windowFlags, cubeColor);
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
