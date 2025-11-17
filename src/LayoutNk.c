e094953ed66bc9dfea0489e4ec794aa1d8ad9324
RafaDEV99 <162469453+RafaDEV99@users.noreply.github.com>
2025-11-17 15:52:10 +0100

    Added a test timer and a nuklear window

--- src/LayoutNk.c
+++ src/LayoutNk.c
@@ -1,5 +1,7 @@
 #include <raylib.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define RAYLIB_NUKLEAR_IMPLEMENTATION
 #include <raylib-nuklear.h>
@@ -9,17 +11,41 @@
 #define WINDOW_HEIGHT   700
 #define FONT_SIZE       14
 
+struct nk_context *ctx;
+float delta = 0.0f;
+
 int main()
 {
-
-    int fontSize = FONT_SIZE;
-
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CrraBloom Simulator!");
     SetTargetFPS(60);
 
+    int fontSize = FONT_SIZE;
+
+    ctx = InitNuklear(fontSize);
+
+
     while (!WindowShouldClose()) {
+
+        delta = GetFrameTime();
+        float time = GetTime();
+        UpdateNuklear(ctx);
+        printf("Time %f\n", time);
+
+        if (time >= 10)
+        {
+            return 0;
+        }
+
+        if (nk_begin(ctx, "My window", nk_rect(20, 75, 220, 220), 
+            NK_WINDOW_BORDER|NK_WINDOW_MINIMIZABLE|NK_WINDOW_MOVABLE))
+        {
+            // TODO: Do something...
+        }
+        nk_end(ctx);
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawNuklear(ctx);
         EndDrawing();
     }
 
