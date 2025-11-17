71806cc02bb90c91b095d082470f1fecb55b8cb7
RafaDEV99 <162469453+RafaDEV99@users.noreply.github.com>
2025-11-17 23:18:02 +0100

    Added more things over there

--- src/LayoutNk.c
+++ src/LayoutNk.c
@@ -20,6 +20,9 @@ int main()
     SetTargetFPS(60);
 
     int fontSize = FONT_SIZE;
+    bool colorCicle = true;
+
+    float time = 5.0f;
 
     ctx = InitNuklear(fontSize);
     Color randomColor;
@@ -27,24 +30,24 @@ int main()
     while (!WindowShouldClose()) {
 
         delta = GetFrameTime();
-        float time = GetTime();
+        time -= delta;
         UpdateNuklear(ctx);
-        printf("Time %f\n", time);
+        printf("Color Cicle: %s\n", colorCicle? "True" : "False");
 
-        randomColor = (Color){GetRandomValue(0, 255), GetRandomValue(0, 255), GetRandomValue(0, 255), 255};
-
-        if (time >= 10)
+        if (time >= 0.0f)
         {
-            // Memory free escape:
-            CloseWindow();
-            UnloadNuklear(ctx);
-            return 0;
+            printf("Time %f\n", time);
+        } 
+        else
+        {
+            printf("Timer out!\n");
+            time = 0.0f;
         }
 
         if (nk_begin(ctx, "My window", nk_rect(20, 75, 220, 220), 
-            NK_WINDOW_BORDER|NK_WINDOW_MINIMIZABLE|NK_WINDOW_MOVABLE))
+                     NK_WINDOW_BORDER|NK_WINDOW_MINIMIZABLE|NK_WINDOW_MOVABLE))
         {
-            nk_layout_row_static(ctx, 50, 120, 1);
+            nk_layout_row_static(ctx, 50, 160, 1);
             nk_label_colored(ctx, "This text is going crazy!!", NK_TEXT_ALIGN_LEFT, ColorToNuklear(randomColor));
         }
         nk_end(ctx);
